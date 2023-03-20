b201954a1e56743f89b95e2e86854d19c8b946c0
Tien-Hung <xiaotianhong@skys-Air.local>
2023-03-20 14:46:02 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,12 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** This method generates number from n. If n is even, it returns n/2. If n is odd, it returns 3n+1 */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
