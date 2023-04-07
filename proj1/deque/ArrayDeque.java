99d6e03ec3799f9d3a15347623b6dd061ab1faf8
Tien-Hung <xiaotianhong@skys-Air.local>
2023-04-07 16:58:16 +0800

    Complete addFirst, addLast and printDeque methods without resizing

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -5,16 +5,29 @@ public class ArrayDeque<Item> {
     private int size;
     private int nextfirst;
     private int nextlast;
+    private int array_size = 8;
     public ArrayDeque() {
-        items = (Item[]) new Object[8];
+        items = (Item[]) new Object[array_size];
         nextfirst = 4;
         nextlast = 5;
         size = 0;
     }
 
-//    public void addFirst(Item item) {
-//
-//    }
+    public void addFirst(Item item) {
+        items[nextfirst--] = item;
+        if(nextfirst < 0){
+            nextfirst = array_size - 1;
+        }
+        size++;
+    }
+
+    public void addLast(Item item) {
+        items[nextlast++] = item;
+        if(nextlast >= array_size){
+            nextlast = 0;
+        }
+        size++;
+    }
 
     public boolean isEmpty() {
         return size == 0;
@@ -22,5 +35,14 @@ public class ArrayDeque<Item> {
 
     public int size() {
         return size;
+    }
+
+    public void printDeque() {
+        int cur = (nextfirst + 1) % array_size;
+        while(cur != nextlast) {
+            System.out.print(items[cur] + " ");
+            cur = (cur + 1) % array_size;
+        }
+        System.out.print("\n");
     }
 }
