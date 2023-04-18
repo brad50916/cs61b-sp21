176be089f073373cc9fc29c0e1f5c9be1567e3b9
Tien-Hung <xiaotianhong@skys-Air.local>
2023-04-18 20:00:15 +0800

    Find old Collatz file

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,22 +4,11 @@
 public class Collatz {
 
     /** This method generates number from n. If n is even, it returns n/2. If n is odd, it returns 3n+1 */
-    /** public static int nextNumber(int n) {
+    public static int nextNumber(int n) {
         if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
             return n / 2;
-        }
-    } */
-
-	/** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
 
