b266dfa34ce99cf9ac6a0e84b3982723c8e39129
Tien-Hung <xiaotianhong@skys-Air.local>
2023-05-02 01:02:30 +0800

    Done with creating folder and file

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,6 +1,8 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
+
 import static capers.Utils.*;
 
 /** A repository for Capers 
@@ -18,8 +20,9 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = Utils.join(".capers");
+    static final File DOGS_FOLDER = Utils.join(".capers", "dogs");
+    static final File STORY_PATH = Utils.join(".capers", "story");
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -31,7 +34,13 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdir();
+        DOGS_FOLDER.mkdir();
+        try {
+            STORY_PATH.createNewFile();
+        } catch (IOException excp) {
+            System.out.println("has existed");
+        }
     }
 
     /**
