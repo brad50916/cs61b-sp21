90ada6e0b8ca847515b6b6a7130cc68489671fd0
Tien-Hung <xiaotianhong@skys-Air.local>
2023-05-23 11:38:47 +0800

    Add IllegalArgumentException in Main mathod

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,5 +1,7 @@
 package gitlet;
-
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author TODO
  */
@@ -9,16 +11,18 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Must have at least one argument");
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                // TODO: handle the `init` command
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
-            // TODO: FILL THE REST IN
+            default:
+                throw new IllegalArgumentException(String.format("Unknown command: %s", args[0]));
         }
     }
 }
