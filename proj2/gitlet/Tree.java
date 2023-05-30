c0dfc57a8c16e87304775d1b3e4e461dae7f18c2
Tien-Hung <xiaotianhong@skys-Air.local>
2023-05-30 10:41:56 +0800

    Add branch hashmap to tree class

--- proj2/gitlet/Tree.java
+++ proj2/gitlet/Tree.java
@@ -7,13 +7,16 @@ import java.util.Map;
 
 public class Tree implements Serializable {
     private TreeNode root;
-    private TreeNode master;
+    private String curBranch;
+    /** HashMap's key is branch name, value is TreeNode */
+    private HashMap<String, TreeNode> branch;
     private TreeNode head;
     private int size = 0;
     public Tree() {
         this.root = null;
-        this.master = null;
         this.head = null;
+        curBranch = "master";
+        this.branch.put(curBranch, null);
     }
     public class TreeNode implements Serializable{
         private String commitSHA;
@@ -33,18 +36,13 @@ public class Tree implements Serializable {
     public void put(String commit) {
         if (this.root == null) {
             this.root = new TreeNode(commit);
-            this.master = root;
+            this.branch.put(curBranch, root);
             this.head = root;
         } else {
             TreeNode temp = new TreeNode(commit);
-            if (head.equals(master)) {
-                this.master.getChildren().put(commit, temp);
-                this.master = master.getChildren().get(commit);
-                this.head = master;
-            } else {
-                this.head.getChildren().put(commit, temp);
-                this.head = head.getChildren().get(commit);
-            }
+            this.branch.get(curBranch).getChildren().put(commit, temp);
+            this.branch.put(curBranch, temp);
+            this.head = temp;
         }
         size++;
     }
