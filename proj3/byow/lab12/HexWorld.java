bceef1f42b3c16786aa2e90c1b98db69f1f13a40
Tien-Hung <xiaotianhong@skys-Air.local>
2023-06-20 23:01:59 +0800

    Add randomTile method

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -14,15 +14,37 @@ import java.util.Random;
 public class HexWorld {
     private static final int WIDTH = 50;
     private static final int HEIGHT = 50;
-    private static void addHexagon(int s, TETile[][] world, int x, int y) {
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+    private static void addHexagon(int s, TETile[][] world, int x, int y, TETile patern) {
         int length = s + 2 * (s - 1);
         int count = 0;
         for (int j = y + s - 1; j >= y; j -= 1) {
             for (int i = x + count; i < x + length - count; i += 1) {
-                world[i][j] = Tileset.FLOWER;
-                world[i][j + 2 * count + 1] = Tileset.FLOWER;
+                world[i][j] = patern;
+                world[i][j + 2 * count + 1] = patern;
+//                Random r = new Random();
+//                world[i][j] = TETile.colorVariant(Tileset.FLOWER, 200, 200, 200, r);
+//                world[i][j + 2 * count + 1] = TETile.colorVariant(Tileset.FLOWER, 200, 200, 200, r);
             }
             count++;
+        }
+    }
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(11);
+        switch (tileNum) {
+            case 0: return Tileset.WALL;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.AVATAR;
+            case 3: return Tileset.FLOOR;
+            case 4: return Tileset.GRASS;
+            case 5: return Tileset.WATER;
+            case 6: return Tileset.LOCKED_DOOR;
+            case 7: return Tileset.UNLOCKED_DOOR;
+            case 8: return Tileset.SAND;
+            case 9: return Tileset.MOUNTAIN;
+            case 10: return Tileset.TREE;
+            default: return Tileset.NOTHING;
         }
     }
     public static void main(String[] args) {
@@ -37,8 +59,9 @@ public class HexWorld {
                 world[x][y] = Tileset.NOTHING;
             }
         }
+
         // draw hexagon
-        addHexagon(5, world, 20, 20);
+        addHexagon(5, world, 20, 20, randomTile());
 
         // draws the world to the screen
         ter.renderFrame(world);
